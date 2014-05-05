99a56a5618d04be45d4a70e35a70ae0c806e9639
laodouya <laodouya@yeah.net>
2014-05-05 18:00:32 +0800

    finish daemon demo

--- daemon.c
+++ daemon.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<string.h>
 #include<fcntl.h>
+#include<signal.h>
 #include<sys/types.h>
 #include<unistd.h>
 #include<sys/wait.h>
@@ -12,36 +13,44 @@ volatile sig_atomic_t _running = 1;
 int main()
 {
     pid_t pc,pid;
-    int i,fd,len,flag = 1;
-    char *buf="this is a Dameon\n";
-    len = strlen(buf);
+    int i,len;
     pc = fork(); //第一步
     if(pc<0){
         printf("error fork\n");
         exit(1);
     }
     else if(pc>0)
-        exit(0);
-    pid = setsid(); //第二步[1]
+        exit(0);  //kill father
+    
+    pid = setsid(); //第二步
     if (pid < 0)
         perror("setsid error");
-    chdir("/"); //第三步
-    umask(0); //第四步
-    for(i=0;i<MAXFILE;i++) //第五步
+
+    pc = fork(); //第三步
+    if(pc<0){
+        printf("error fork\n");
+        exit(1);
+    }
+    else if(pc>0)
+        exit(0);  //kill father
+    
+    for(i=0;i<MAXFILE;i++) //第四步
         close(i);
-    signal(SIGTERM, sigterm_handler);
+ 
+    chdir("/"); //第五步
+    umask(0); //第六步
+    
+    signal(SIGCHLD,SIG_IGN); //第七步
+    
+    signal(SIGTERM, sigterm_handler); //第八步
+    
     while( _running )
     {
-        if( flag ==1 &&(fd=open("/tmp/daemon.log",O_CREAT|O_WRONLY|O_APPEND,0600))<0)
-        {
-            perror("open");
-            flag=0;
-            exit(1);
-        }
-        write(fd,buf,len);
-        close(fd);
-        usleep(10*1000); //10毫秒
+        //TODO Your thing.
+        sleep(100);
     }
+    
+    return 0;
 }
 void sigterm_handler(int arg)
 {
