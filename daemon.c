6c54b0e099fa739ea1d85360e72d53a9a163c9cc
root <root@laodouya.lenovo>
2014-05-07 17:59:21 +0800

    make daemon keep server.js running

--- daemon.c
+++ daemon.c
@@ -10,49 +10,69 @@
 #define MAXFILE 65535
 void sigterm_handler(int arg);
 volatile sig_atomic_t _running = 1;
-int main()
+void make_it_daemon()
 {
-    pid_t pc,pid;
+    pid_t pid;
     int i,len;
-    pc = fork(); //第一步
-    if(pc<0){
+    pid = fork(); //第一步
+    if(pid<0){
         printf("error fork\n");
         exit(1);
     }
-    else if(pc>0)
+    else if(pid>0)
         exit(0);  //kill father
-    
+
     pid = setsid(); //第二步
     if (pid < 0)
         perror("setsid error");
 
-    pc = fork(); //第三步
-    if(pc<0){
+    pid = fork(); //第三步
+    if(pid<0){
         printf("error fork\n");
         exit(1);
     }
-    else if(pc>0)
+    else if(pid>0)
         exit(0);  //kill father
-    
+
     for(i=0;i<MAXFILE;i++) //第四步
         close(i);
- 
+
     chdir("/"); //第五步
     umask(0); //第六步
-    
+
     signal(SIGCHLD,SIG_IGN); //第七步
-    
+
     signal(SIGTERM, sigterm_handler); //第八步
-    
+}
+
+int main()
+{
+    int status;
+    pid_t pid;
+    make_it_daemon();
     while( _running )
     {
-        //TODO Your thing.
-        sleep(100);
-    }
-    
-    return 0;
-}
-void sigterm_handler(int arg)
-{
-    _running = 0;
+        //TODO if server.js is not running
+        pid = fork();
+        if (pid<0)
+        {
+            printf("error fork");
+            exit(1);
+        }
+        else if (0==pid)
+        {
+            int ret = execl("/usr/bin/node","node","/var/www/Paomianba/src/plugin/replit/server.js",NULL);
+            if (ret<0)
+                perror("execl server.js");
+            exit(1);
+        }
+
+        waitpid(pid,&status,0);
+
+    } 
+    return 0; 
+} 
+void sigterm_handler(int arg) 
+{ 
+    _running = 0; 
 }
